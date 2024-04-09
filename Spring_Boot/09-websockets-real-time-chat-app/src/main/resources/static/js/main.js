f57ae9de850931220daf65c421224bfb310af65e
AlbertAlvi <albertalvin99@gmail.com>
2024-04-09 20:03:31 -0300

    shell-driven-update

--- Spring_Boot/09-websockets-real-time-chat-app/src/main/resources/static/js/main.js
+++ Spring_Boot/09-websockets-real-time-chat-app/src/main/resources/static/js/main.js
@@ -1,16 +1,24 @@
 "use strict";
 
+const formJoin = document.querySelector("#joinForm");
 const btnJoin = document.querySelector("#join");
-const btnLeave = document.querySelector("#leave");
+const inputUserName = document.querySelector("#username");
+
+// const btnLeave = document.querySelector("#leave");
 const btnSendMessage = document.querySelector("#sendMessage");
 const inputMessage = document.querySelector("#message");
+
+const chatContainer = document.querySelector(".chat-container");
+const chatHistory = document.querySelector(".chat-history");
 
 let stompClient = null;
 let sockJS = null;
 
 btnJoin.addEventListener("click", joinChat, true);
+
 function joinChat(event)
 {
+    event.preventDefault();
     sockJS = new SockJS("/ws");
     stompClient = Stomp.over(sockJS);
     stompClient.connect({}, onConnect, onError);
@@ -19,16 +27,40 @@ function joinChat(event)
 function onConnect()
 {
     stompClient.subscribe("/topic/public", onMessageReceived);
+
+    const username = inputUserName.value.trim();
     stompClient.send(
         "/app/chat.addUser",
         {},
-        JSON.stringify({sender: "Lucasss", type: "JOIN"})
+        JSON.stringify({sender: username, messageType: "JOIN"})
     );
+
+    formJoin.classList.add("hidden");
+    chatContainer.classList.remove("hidden");
 }
 
 function onMessageReceived(payload)
 {
-    console.log(payload.body);
+    const chatMessage = JSON.parse(payload.body);
+
+    let newMessage = document.createElement("div");
+
+    if (chatMessage.messageType === "JOIN") {
+        newMessage.innerHTML = `&gt;<strong>${chatMessage.sender}</strong>&lt;  joined the chat`;
+        newMessage.classList.add("received-message");
+    }
+    else if (chatMessage.messageType === "LEAVE") {
+        newMessage.innerHTML = `&gt;<strong>${chatMessage.sender}</strong>&lt;  left the chat`;
+        newMessage.classList.add("received-message");
+    }
+    else if(chatMessage.messageType === "CHAT") {
+        newMessage.innerHTML = `<div><strong>${chatMessage.sender}</div></strong>${chatMessage.content}`;
+    }
+
+    if(inputUserName.value.trim() === chatMessage.sender)
+        newMessage.classList.add("user-message");
+
+    chatHistory.appendChild(newMessage);
 }
 
 function onError()
@@ -37,13 +69,18 @@ function onError()
 }
 
 btnSendMessage.addEventListener("click", sendMessage, true);
-function sendMessage()
+
+function sendMessage(event)
 {
+    event.preventDefault();
+
+    const username = inputUserName.value.trim();
     const message = inputMessage.value.trim();
+
     const chatMessage = {
         content: message,
-        sender: "Lucasss",
-        type: "CHAT"
+        sender: username,
+        messageType: "CHAT"
     }
 
     stompClient.send(
