91519ac4940981e8c280ed3653c8c10123ee9900
AlbertAlvi <albertalvin99@gmail.com>
2024-04-18 17:59:13 -0300

    shell-driven-update

--- Spring_Reactive/07-spring-webflux-essentials/src/main/java/org/example/springwebfluxessentials/controllers/ProductController.java
+++ Spring_Reactive/07-spring-webflux-essentials/src/main/java/org/example/springwebfluxessentials/controllers/ProductController.java
@@ -2,7 +2,7 @@ package org.example.springwebfluxessentials.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.example.springwebfluxessentials.model.Product;
-import org.example.springwebfluxessentials.repositories.ProductRepository;
+import org.example.springwebfluxessentials.services.ProductService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +13,10 @@ import reactor.core.publisher.Flux;
 @RequestMapping("/product")
 public class ProductController {
 
-    private final ProductRepository productRepository;
+    private final ProductService productService;
 
-    @GetMapping
+    @GetMapping("/all")
     public Flux<Product> findAll() {
-        return productRepository.findAll();
+        return productService.findAll();
     }
 }
--- Spring_Reactive/07-spring-webflux-essentials/src/main/java/org/example/springwebfluxessentials/services/ProductService.java
+++ Spring_Reactive/07-spring-webflux-essentials/src/main/java/org/example/springwebfluxessentials/services/ProductService.java
@@ -1,11 +1,17 @@
 package org.example.springwebfluxessentials.services;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springwebfluxessentials.model.Product;
 import org.example.springwebfluxessentials.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+
+    public Flux<Product> findAll() {
+        return productRepository.findAll();
+    }
 }
