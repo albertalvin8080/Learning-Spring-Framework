1fd12c114ef1ad8df707e1aa387fc15acd90c669
AlbertAlvi <albertalvin99@gmail.com>
2024-04-24 20:15:05 -0300

    shell-driven-update

--- Spring-Microservices/00-spring-microservices-essentials/product/src/main/java/com/albert/product/ProductApplication.java
+++ Spring-Microservices/00-spring-microservices-essentials/product/src/main/java/com/albert/product/ProductApplication.java
@@ -8,11 +8,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EntityScan(basePackages = {"com.albert.core.model"})
 @EnableJpaRepositories(basePackages = {"com.albert.core.repositories"})
-public class Application
+public class ProductApplication
 {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ProductApplication.class, args);
     }
 
 }
