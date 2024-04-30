de61673241a9283ccd7b2ad0804147442efd2421
AlbertAlvi <albertalvin99@gmail.com>
2024-04-30 19:30:55 -0300

    shell-driven-update

--- Spring-Microservices/01-microservices-product-catalog/order-service/src/main/java/com/albert/orderservice/controllers/OrderController.java
+++ Spring-Microservices/01-microservices-product-catalog/order-service/src/main/java/com/albert/orderservice/controllers/OrderController.java
@@ -2,9 +2,11 @@ package com.albert.orderservice.controllers;
 
 import com.albert.core.dto.OrderDto;
 import com.albert.orderservice.services.OrderService;
+
+import brave.Span;
+import brave.Tracer;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
-import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,17 +16,27 @@ import java.util.concurrent.CompletableFuture;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/order")
-public class OrderController
-{
+public class OrderController {
+    private final Tracer tracer;
     private final OrderService service;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name = "inventory", fallbackMethod = "inventoryFailureFallback")
-//    @TimeLimiter(name = "inventory")
+    // @TimeLimiter(name = "inventory")
     @Retry(name = "inventory")
     public CompletableFuture<String> placeOrder(@RequestBody OrderDto orderDto) {
-        return CompletableFuture.supplyAsync(() -> service.placeOrder(orderDto));
+        return CompletableFuture.supplyAsync(() -> {
+            // Used for joining the span of the current request with the response of this CompletableFuture<>.
+            Span currentSpan = tracer.currentSpan();
+            Span childSpan = tracer.newChild(currentSpan.context()).name("placeOrder");
+
+            try(Tracer.SpanInScope ws = tracer.withSpanInScope(childSpan.start())) {
+                return service.placeOrder(orderDto);
+            } finally {
+                childSpan.finish();
+            }
+        });
     }
 
     private CompletableFuture<String> inventoryFailureFallback(OrderDto orderDto, RuntimeException runtimeException) {
